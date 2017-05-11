5fd488b3a8fdc8bcd2851d0e05459ee69204f785
Itsik <Itsik@Itsik-PC>
2017-05-11 20:53:28 +0300

    Coupon System Core

---
+++ src/testcs/Main.java
@@ -0,0 +1,19 @@
+package testcs;
+
+import a.db.create.BuildDB;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Test.testCs();
+		//Test.delete();
+		
+		//Test2.TestCSFailures();
+		
+		BuildDB.createDb();
+		System.out.println("done");
+		
+	}
+
+}
