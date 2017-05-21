b3322b7f921ce4d9e09e47780c859cfd0de8ab93
Itsik <Itsik@Itsik-PC>
2017-05-21 11:53:52 +0300

    for hand in

--- src/testcs/Main.java
+++ src/testcs/Main.java
@@ -6,13 +6,18 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//Test.testCs();
+		
 		//Test.delete();
 		
 		//Test2.TestCSFailures();
 		
+		
+		//create the data base then comment this line and continue
 		BuildDB.createDb();
 		System.out.println("done");
+		
+		//after DB is built uncomment the below and run
+		//Test.testCs();
 		
 	}
 
